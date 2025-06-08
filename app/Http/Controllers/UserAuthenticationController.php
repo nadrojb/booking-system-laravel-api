93dc8ffefee370bcd6aadef260542e575269b3cc
nadrojb <jordanbrewer1994@gmail.com>
2025-06-08 12:58:53 +0100

    First commit

---
+++ app/Http/Controllers/UserAuthenticationController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\LoginUserRequest;
+use App\Http\Requests\Api\StoreOrUpdateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+class UserAuthenticationController extends Controller
+{
+
+    public function login(LoginUserRequest $request): JsonResponse
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return $this->error('Invalid credentials', 401);
+        }
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user) {
+            return $this->error('User not found', 404);
+        }
+
+        return $this->ok(
+            'Authenticated',
+            [
+                'token' => $user->createToken(
+                    'API token for' . $user->email,
+                    ['*'],
+                    now()->addMonth())->plainTextToken
+            ]
+        );
+    }
+
+    public function register(StoreOrUpdateUserRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        return new UserResource($user);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return $this->ok('');
+    }
+}
